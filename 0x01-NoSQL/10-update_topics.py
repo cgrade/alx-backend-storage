fa5786cda679137525d9568d754add9b9ba1d6ea
cgrade <gradeisageek@gmail.com>
2023-08-14 19:09:38 -0700

    correction

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -11,4 +11,5 @@ by changing all the topics of the doc with name
 
 def update_topics(mongo_collection, name, topics):
     """A func that update a collec in mongodb"""
-    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    return mongo_collection.update_many(
+            {"name": name}, {"$set": {"topics": topics}})
