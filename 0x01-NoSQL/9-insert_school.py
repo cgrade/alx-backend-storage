b49e3a1c794c6dc3eceb99f4cf347fb2dd1b96f3
cgrade <gradeisageek@gmail.com>
2023-08-14 18:54:13 -0700

    solution to task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A python function that inserts a new document
+   in a collection based on Kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """func that insert document in a MongoDB collection"""
+    mongo_collection.insert_many(kwargs)
+    return mongo_collection.inserted_id
