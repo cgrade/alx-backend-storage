a5254d8eabd10e64e9edeb4f7be2d5aadb9685a0
cgrade <gradeisageek@gmail.com>
2023-08-14 19:05:07 -0700

    solution to task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+"""a script that updates a collecion in mongodb
+by changing all the topics of the doc with name
+
+   Takse 3 args:
+   Arg1: Mongo_collection
+   Arg2: name
+   Arg3: topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """A func that update a collec in mongodb"""
+    return mongo_collection.update(name, topics)
